6decba5637e6adfba1f1f63ae53e67a0a4c710bd
Savin Osuka <44639335+savin2001@users.noreply.github.com>
2021-01-05 20:47:33 +0300

    File upload

---
+++ build/precache-manifest.1f565bab4c187e0009d0977a8ec972d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "543a3eedbcc9b83b5fb29f23bff5f691",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7a52c58a4253a162257c",
+    "url": "/static/css/main.91c635a9.chunk.css"
+  },
+  {
+    "revision": "5381e8fc30c2c68af348",
+    "url": "/static/js/2.b4e3e591.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.b4e3e591.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a52c58a4253a162257c",
+    "url": "/static/js/main.47bc55d6.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
